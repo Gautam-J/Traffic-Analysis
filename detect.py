491e5094faf14e76d6755b4339d3118939558506
Gautam J <gautam.jayapal@gmail.com>
2021-05-23 11:41:43 +0530

    add detect.py

---
+++ detect.py
@@ -0,0 +1,181 @@
+import cv2
+import time
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.python.saved_model import tag_constants
+
+import core.utils as utils
+from core.config import cfg
+
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+
+WEIGHTS = './checkpoints/yolov4-tiny-416'
+IMG_SIZE = 416
+CONF_THRESHOLD = 0.5  # object confidence threshold
+IOU_THRESHOLD = 0.45  # IOU threshold for NMS
+VIDEO_PATH = 'data/cut.mp4'
+MAX_COSINE_DISTANCE = 0.4
+NN_BUDGET = None
+NMS_MAX_OVERLAP = 1.0
+
+# configure GPU
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# initialize deep sort
+model_filename = 'model_data/mars-small128.pb'
+encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+# calculate cosine distance metric
+metric = nn_matching.NearestNeighborDistanceMetric("cosine", MAX_COSINE_DISTANCE, NN_BUDGET)
+# initialize tracker
+tracker = Tracker(metric)
+
+# load configuration for object detector
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config()
+
+saved_model_loaded = tf.saved_model.load(WEIGHTS, tags=[tag_constants.SERVING])
+infer = saved_model_loaded.signatures['serving_default']
+
+# read in all class names from config
+class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+allowed_classes = [
+    'car',
+    'motorcycle',
+    'bus',
+    'truck',
+]
+
+# initialize color map
+cmap = plt.get_cmap('tab20b')
+colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+videoCapture = cv2.VideoCapture(VIDEO_PATH)
+width = int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(videoCapture.get(cv2.CAP_PROP_FPS))
+codec = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('data/out.avi', codec, fps, (width, height))
+
+while True:
+    ret, frame = videoCapture.read()
+
+    if ret:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(frame)
+
+        image_data = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+        start_time = time.time()
+
+        batch_data = tf.constant(image_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=IOU_THRESHOLD,
+            score_threshold=CONF_THRESHOLD
+        )
+
+        # convert data to numpy arrays and slice out unused elements
+        num_objects = valid_detections.numpy()[0]
+        bboxes = boxes.numpy()[0]
+        bboxes = bboxes[0:int(num_objects)]
+        scores = scores.numpy()[0]
+        scores = scores[0:int(num_objects)]
+        classes = classes.numpy()[0]
+        classes = classes[0:int(num_objects)]
+
+        # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+        original_h, original_w, _ = frame.shape
+        bboxes = utils.format_boxes(bboxes, original_h, original_w)
+
+        # store all predictions in one parameter for simplicity when calling functions
+        pred_bbox = [bboxes, scores, classes, num_objects]
+
+        # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+        names = []
+        deleted_indx = []
+        for i in range(num_objects):
+            class_indx = int(classes[i])
+            class_name = class_names[class_indx]
+            if class_name not in allowed_classes:
+                deleted_indx.append(i)
+            else:
+                names.append(class_name)
+        names = np.array(names)
+        count = len(names)
+
+        # print('[DEBUG] Total number of Objects tracked:', count)
+
+        # delete detections that are not in allowed_classes
+        bboxes = np.delete(bboxes, deleted_indx, axis=0)
+        scores = np.delete(scores, deleted_indx, axis=0)
+
+        # encode yolo detections and feed to tracker
+        features = encoder(frame, bboxes)
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+
+        # run non-maxima supression
+        boxs = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        classes = np.array([d.class_name for d in detections])
+        indices = preprocessing.non_max_suppression(boxs, classes, NMS_MAX_OVERLAP, scores)
+        detections = [detections[i] for i in indices]
+
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+
+        # update tracks
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlbr()
+            class_name = track.get_class()
+
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1] - 30)), (int(bbox[0]) + (len(class_name) + len(str(track.track_id))) * 17, int(bbox[1])), color, -1)
+            cv2.putText(frame, class_name + "-" + str(track.track_id), (int(bbox[0]), int(bbox[1] - 10)), 0, 0.75, (255, 255, 255), 2)
+
+        fps = 1.0 / (time.time() - start_time)
+        print(f'[DEBUG] Total number of vehicles {count} | FPS {fps:.2f}')
+        result = np.asarray(frame)
+        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+        out.write(result)
+        cv2.imshow('yolov4 with Deep Sort', result)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        break
+
+videoCapture.release()
+out.release()
+cv2.destroyAllWindows()
+
+print('Saved Processed Video')
